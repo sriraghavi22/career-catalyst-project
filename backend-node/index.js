0733f9aeee9fc0f265c45a4bbdec24bc1a51ae3c
sriraghavi22 <raghavi5417@gmail.com>
2025-05-28 15:06:41 +0530

    updated index.js

--- backend-node/index.js
+++ backend-node/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://career-catalyst-six.vercel.app'],
+  origin: ['http://localhost:5173', 'https://career-catalyst-project.vercel.app'],
   methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
