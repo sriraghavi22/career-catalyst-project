2a6aebba661f5bb2b5d295011d95353fb5613dc3
sriraghavi22 <140194099+sriraghavi22@users.noreply.github.com>
2025-05-29 18:20:07 +0530

    Update index.js

--- backend-node/index.js
+++ backend-node/index.js
@@ -13,7 +13,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: ['http://localhost:5173', 'https://career-catalyst-project.vercel.app'],
-  methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
